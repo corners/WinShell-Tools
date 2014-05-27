7ce76da6359abba3c20474b4760dcf1bb9555605
Dan Ward <dan.j.ward@gmail.com>
2014-05-27 09:54:56 +0100

    Moved some tools into dev and added WorkRave

--- cinst-dev.cmd
+++ cinst-dev.cmd
@@ -1,14 +1,24 @@
 echo Dev tools
+cinst git
+cinst githubforwindows
+cinst SublimeText3
+cinst vim
+cinst nodejs.install
 cinst ilspy
 cinst fiddler4 
 cinst wireshark
-cinst SublimeText3
-cinst vim
 cinst ctags
-cinst nodejs.install
-cinst couchdb
-cinst githubforwindows
+cinst wudt
+cinst linqpad4
+cinst hxd
+cinst curl 
+cinst putty
+cinst bleachbit 
+cinst expresso
+cinst markdownpad2
 
-cinst webpi
-cinst VisualStudio2012Premium
-cinst WebEssentials2012
+
+::cinst couchdb
+::cinst webpi
+::cinst VisualStudio2012Premium
+::cinst WebEssentials2012
